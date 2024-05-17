46577ad14c65d562b9dd5c9b5abe0c54399d83e0
DeoSeptry <deoseptry9@gmail.com>
2024-05-17 18:01:43 +0700

    ah

--- src/redux/action/dataAction.js
+++ src/redux/action/dataAction.js
@@ -8,7 +8,7 @@ import {
 
 export const getQuran = () => async (dispatch, getState) => {
   try {
-    const response = await axios.get(`http://api.alquran.cloud/v1/surah`);
+    const response = await axios.get(`https://api.alquran.cloud/v1/surah`);
     console.log("REDUX response", response.data.data);
     dispatch(setSurah(response.data.data));
   } catch (error) {
@@ -24,7 +24,7 @@ export const getQuranDetail = () => async (dispatch, getState) => {
   try {
     const id = getState().data.surahId;
     const response = await axios.get(
-      `http://api.alquran.cloud/v1/surah/${id}/ar.alafasy`
+      `https://api.alquran.cloud/v1/surah/${id}/ar.alafasy`
     );
     console.log("REDUX detail", response.data.data);
     dispatch(setDetail(response.data.data));
@@ -43,7 +43,7 @@ export const getTerjemahan = () => async (dispatch, getState) => {
     console.log("bahasa", bahasa);
     const id = getState().data.surahId;
     const response = await axios.get(
-      `http://api.alquran.cloud/v1/surah/${id}/${bahasa}`
+      `https://api.alquran.cloud/v1/surah/${id}/${bahasa}`
     );
     console.log("REDUX arti", response.data.data);
     dispatch(setArti(response.data.data));
@@ -60,7 +60,7 @@ export const getAyah = () => async (dispatch, getState) => {
   try {
     const nomor = getState().data.nomor;
     const response = await axios.get(
-      `http://api.alquran.cloud/v1/ayah/${nomor}/ar.alafasy`
+      `https://api.alquran.cloud/v1/ayah/${nomor}/ar.alafasy`
     );
     console.log("REDUX nomor", response.data.data);
     dispatch(setEditAyah(response.data.data));
